2aac26782a388b0ffaff6d0d335a96567ebf49cc
reza <rezadadgar.py@lgmail.com>
2026-05-22 00:42:03 +0330

    cleanup project

---
+++ website/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse, JsonResponse
+import json
+
+
+def index_view(request):
+    return HttpResponse("<h1>Home Page</h1>")
+
+def about_view(request):
+    return HttpResponse("about page")
+
+def contact_view(request):
+    return HttpResponse("<h1>Contact Page</h1>")
+
