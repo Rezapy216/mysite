060533ab580309cd55888c51640a61b57e276b6f
reza <rezadadgar.py@lgmail.com>
2026-05-29 22:45:33 +0330

    .

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,13 @@
 
 from django.urls import path
 from website.views import *
+app_name = 'website'
 urlpatterns = [
 
-    path("", index_view),
+    path("", index_view, name='index'),
     path("about", about_view),
-    path("contact", contact_view)
-
+    path("contact", contact_view),
+    path('index', index_view),
+    path('elements', elements_view),
+    path('test',test_view, name='test')
 ]
