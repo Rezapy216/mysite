b0642c7b0f7c9e42df7ef56fbf21c0eb3c852287
reza's pc02 <rezagholamidadgar2896@gmail.com>
2026-06-27 14:06:08 +0330

    feat: set get_object_or_404 for single blog to return 404 eror is post dose nt exists

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 def blog_home_view(request):
-    return render(request, "blog/blog-home.html")
+    post = Post.objects.filter(status=1)
+    context = {'post': post}
+    return render(request, "blog/blog-home.html", context)
 
-
-def blog_single_view(request):
-    context = {'title': "bitcoin is gooooood", "content": 'test content'}
+def blog_single_view(request, pid):
+    post = get_object_or_404(Post, pk=pid)
+    context ={'post': post}
     return render(request, "blog/blog-single.html", context)
 
+def test(request, pid):
+    post = Post.objects.filter(pk=pid)
+    context = {'post': post}
+    return render(request, 'test.html', context)
