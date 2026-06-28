d8e69715634f5dfbe2b72b499a46a5b182b75c4e
Rezapy216 <137828511+Rezapy216@users.noreply.github.com>
2026-06-28 11:58:46 +0330

    add: list_display and list_filter

--- blog/admin.py
+++ blog/admin.py
@@ -5,5 +5,7 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
     empty_value_display = "-empty-"
+    list_display = ('title', 'author', 'status')
+    list_filter = ('author',)
     
 admin.site.register(Post, PostAdmin)
