172f67c44b967274c1ecae39f4a32cc6ca2f5a23
Rezapy216 <137828511+Rezapy216@users.noreply.github.com>
2026-06-28 12:02:16 +0330

    add: post's author row in databas

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
@@ -8,6 +8,6 @@ class Post(models.Model):
     published_time = models.DateTimeField(null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     def __str__(self):
         return f'{self.id}, {self.title}'
