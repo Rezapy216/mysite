09184708a2cb13bd59c89980303d772bef19d187
Rezapy216 <137828511+Rezapy216@users.noreply.github.com>
2026-06-30 12:00:07 +0330

    add: Category model for each post

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
@@ -9,5 +14,16 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    images = models.ImageField(upload_to='blog/', default='blog/default.jpg')
+    category = models.ManyToManyField(Category)
+
+    class Meta:
+        ordering = ['-created_date']
+
     def __str__(self):
         return f'{self.id}, {self.title}'
+
+    def snippets(self):
+        return self.content[:100] + '...'
+
+
