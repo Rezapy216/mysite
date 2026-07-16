403f052e3e6dee83e14ad26ddffb1a57916cabee
Rezapy216 <137828511+Rezapy216@users.noreply.github.com>
2026-07-16 13:17:14 +0330

    Revert ".."
    This reverts commit 68d9e735e012e70ba02f432973540b0e69d03fd9.

--- website/templatetags/website_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from blog.models import Post
-register = template.Library()
-
-@register.inclusion_tag('website/index-latestposts.html')
-def index_latestposts(arg=6):
-    posts = Post.objects.filter(status=1)
-    return {'posts': posts}
