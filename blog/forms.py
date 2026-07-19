06bca0c96c4b6b52a39e6a34897b0b6c20dddf22
Rezapy216 <137828511+Rezapy216@users.noreply.github.com>
2026-07-19 17:52:00 +0330

    feat: add Contact model and ModelForm with Meta class

---
+++ blog/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class NameForm(forms.Form):
+    name = forms.CharField(max_length=255)
+    email = forms.EmailField()
+    subject = forms.CharField()
+    message = forms.CharField(widget=forms.Textarea())
+
+
