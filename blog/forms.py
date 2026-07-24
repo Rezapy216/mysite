c7b9b4eb432882e9cba2b0f41d36d172d827e4f3
Rezapy216 <137828511+Rezapy216@users.noreply.github.com>
2026-07-24 18:46:33 +0330

    feat(forms): add ContactForm model form for Contact model

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from website.models import Contact
 
 class NameForm(forms.Form):
     name = forms.CharField(max_length=255)
@@ -6,4 +7,8 @@ class NameForm(forms.Form):
     subject = forms.CharField()
     message = forms.CharField(widget=forms.Textarea())
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = "__all__"
 
