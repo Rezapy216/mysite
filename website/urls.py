398c66f5be14243b9aa75b1be24cc47ed4b2eeff
Rezapy216 <137828511+Rezapy216@users.noreply.github.com>
2026-07-24 18:43:07 +0330

    feat(website): add name to contact url pattern

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
     path("", index_view, name='index'),
     path("about", about_view),
-    path("contact", contact_view),
+    path("contact", contact_view, name='contact'),
     path('index', index_view),
     path('elements', elements_view)
 ]
